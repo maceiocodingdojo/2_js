163f4537daf5cdbd2bde9460fff265fd3f111b56
Rachid Calazans <rachidcalazans@gmail.com>
2015-05-26 20:17:29 -0300

    Irineu Filho

--- spec/NumberExtensionSpec.js
+++ spec/NumberExtensionSpec.js
@@ -9,5 +9,11 @@ describe("NumberExtension", function(){
   it("3 must return size 4", function(){
     expect(new NumberExtension().numbers(3).length).toBe(4);
   });
+  it("4 must return size 6", function(){
+    expect(new NumberExtension().numbers(4).length).toBe(6);
+  });
+  it("5 must return size 5", function(){
+    expect(new NumberExtension().numbers(4).length).toBe(6);
+  });
 
 });
--- src/NumberExtension.js
+++ src/NumberExtension.js
@@ -2,8 +2,29 @@ function NumberExtension(){
 
 }
 NumberExtension.prototype.numbers = function(number) {
-	if (number === 1)
-		return "um";
-	else 
-		return "dois";
+	switch(number){
+		case 1:
+			return "um";
+			break;
+		case 2:
+			return "dois";
+			break;
+		case 3:
+			return "tres";
+			break;			
+		case 4:
+			return "quatro";
+			break;
+		case 5:
+			return "quatro";
+			break;
+
+		default:
+			break;
+	}
+
+	// if (number === 1)
+	// 	return "um";
+	// else 
+	// 	return "dois";
 }

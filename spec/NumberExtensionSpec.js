bfcd0f70a87dec0c92eb43b306aeb7eecfea739f
Marina Limeira <marinaflessa@gmail.com>
2015-05-30 09:39:39 -0300

    Finished the conversion to number in words in portuguese

--- spec/NumberExtensionSpec.js
+++ spec/NumberExtensionSpec.js
@@ -8,44 +8,52 @@ describe("NumberExtension", function(){
   describe("when number is simple", function(){
 
 	  it("1 must return size 2", function(){
-	    expect(conversor.numbers(1)).toBe(2);
+	    expect(conversor.numbers(1)).toBe("um".length);
 	  });
-	  it("2 must return size 4", function(){
-	    expect(conversor.numbers(2)).toBe(4);
-	  });
-	  it("3 must return size 4", function(){
-	    expect(conversor.numbers(3)).toBe(4);
-	  });
-	  it("4 must return size 6", function(){
-	    expect(conversor.numbers(4)).toBe(6);
-	  });
-	  it("5 must return size 5", function(){
-	    expect(conversor.numbers(4)).toBe(6);
-	  });
+
 	  it("6 must return size 4", function(){
-	    expect(conversor.numbers(6)).toBe(4);
+	    expect(conversor.numbers(6)).toBe("seis".length);
 	  });
 
 	  it("10 must return size 3", function() {
-	  	expect(conversor.numbers(10)).toBe(3);
+	  	expect(conversor.numbers(10)).toBe("dez".length);
 	  });
+
 	  it("30 must return size 6", function() {
 	  	expect(conversor.numbers(30)).toBe("trinta".length);
 	  });
+
+    it("100 must return size 3", function(){
+      expect(conversor.numbers(100)).toBe("cem".length);
+    });
   });
 
-  it("22 must return 10", function() {
-  	expect(conversor.numbers(22)).toBe(10)
-  })
-  it("99 must return 13", function() {
-  	expect(conversor.numbers(99)).toBe(12)
-  })
+  describe("when number is not simple", function(){
 
-  it("101 must return ", function(){
-  	expect(conversor.numbers(101)).toBe(8)
-  })
-  //it("129 must return ")
+    it("22 must return 10", function() {
+      expect(conversor.numbers(22)).toBe("vinteedois".length);
+    });
 
+    it("99 must return 13", function() {
+      expect(conversor.numbers(99)).toBe("noventaenove".length);
+    });
+
+    it("101 must return 8", function(){
+      expect(conversor.numbers(101)).toBe("centoeum".length);
+    });
+
+    it("513 must return 16", function(){
+      expect(conversor.numbers(513)).toBe("quinhentosetreze".length);
+    });
+
+    it("726 must return size 21", function(){
+      expect(conversor.numbers(726)).toBe("setecentosevinteeseis".length);
+    });
+
+    it ("999 must return 26", function(){
+      expect(conversor.numbers(999)).toBe("novecentosenoventaenove".length);
+    });
+ });
 });
 
 
--- src/NumberExtension.js
+++ src/NumberExtension.js
@@ -1,68 +1,48 @@
 function NumberExtension(){
-
 }
+
 NumberExtension.prototype.numbers = function(number) {
 	
-	var units = {
-	  1:"um",
-	  2:"dois",
-	  3:"tres",
-	  4:"quatro",
-	  5:"cinco",
-	  6:"seis",
-	  7:"sete",
-	  8:"oito",
-	  9:"nove",
-	  10:"dez",
-	  11:"onze",
-	  12:"doze",
-	  13:"treze",
-	  14:"",
-
-	  20:"vinte",
-	  30:"tlinta",
-  	  90:"noventa",
-  	  100: "cem"
-
-
+	var simple = {
+	  1:"um", 2:"dois", 3:"tres", 4:"quatro", 5:"cinco", 6:"seis", 7:"sete", 8:"oito", 9:"nove", 10:"dez",
+	  11:"onze", 12:"doze", 13:"treze", 14:"quatorze", 15:"quinze", 16:"dezesseis", 17:"dezessete",
+	  18:"dezoito", 19:"dezenove", 20:"vinte", 30:"trinta", 40:"quarenta", 50:"cinquenta", 60:"sessenta",
+	  70:"setenta", 80:"oitenta", 90:"noventa", 100:"cem", 200:"duzentos", 300:"trezentos", 400:"quatrocentos", 
+	  500:"quinhentos", 600:"seiscentos", 700:"setecentos", 800:"oitocentos", 900:"novecentos", 1000:"mil"
 	};
 
+	var special = {
+		100:"cento"
+	};
 
-	var hundred = {
-		
-		1:"cento",
-	  	2:"duzentos",
-	  	3:"trezentos",
-	  	4:"quatrocentos",
-	  	5:"quinhentos",
-	 	6:"seiscentos",
-	  	7:"setecentos",
-	  	8:"oitocentos",
-	  	9:"novecentos"
+	if (simple[number] != undefined){
+		console.log(simple[number]);
+		return simple[number].length;
+	} else {
 
-	}
-	
-	if (units[number] != undefined){
-		return units[number].length;
-	}
+		var size = "";
 
-	var tamanho = 0;
+		if ((number > 100) && (number < 1000)){
+			var hundred = parseInt(number/100) * 100;
+			number -= hundred;
+			if (special[hundred] != undefined) {
+				size += (special[hundred]) + "e";
+			} else {
+				size += (simple[hundred]) + "e";
+			}
+		}
 
-	if ((number > 100) && (number <= 999)){
-		var numCen = parseInt(number/100);
-		number -= numCen * 100;
-		tamanho += (hundred[numCen]).length + 1;
-	}
+		if ((number >= 20) && (number < 100)){
+			var ten = parseInt(number / 10) * 10;
+			number -= ten;
+			size += (simple[ten]) + "e"; 
+		}
 
-	if ((number > 20) && (number < 100)){
-		var numDez = parseInt(number / 10) * 10;
-		number -= numDez;
-		tamanho += (units[numDez]).length + 1; 
-	}
+		if (number < 20){
+			size += simple[number];
+		}
 
-	if (number < 10){
-		tamanho += units[number].length;
-	}
-
-	return tamanho;	
+		console.log(size);
+		return size.length;
+	}		
 }
